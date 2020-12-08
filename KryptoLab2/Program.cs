5e5f7ca03f352f6a435c1cdfdd36c3ec5cd09b2a
Pyrohova <pyrohovas@ukr.net>
2020-12-08 20:11:56 +0200

    basic functionality added

--- KryptoLab2/Program.cs
+++ KryptoLab2/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace KryptoLab2
 {
     class MainClass
     {
-        static byte[] XOR(byte[] array1, byte[] array2)
+        static byte[] firstCiphertext;
+        static byte[] secondCiphertext;
+
+        static string cribWord;
+
+        static byte[] XOR(byte[] first, byte[] second)
         {
-            int minLength = (array1.Length > array2.Length) ? array2.Length : array1.Length;
+            int minLength = (first.Length > second.Length) ? second.Length : first.Length;
 
             byte[] result = new byte[minLength];
 
-            for (int i=0; i< minLength; i++)
+            for (int i = 0; i < minLength; i++)
             {
-                result[i] = (byte)(array1[i] ^ array2[i]);
+                result[i] = (byte)(first[i] ^ second[i]);
             }
             return result;
 
@@ -42,9 +48,50 @@ namespace KryptoLab2
             return Encoding.ASCII.GetString(input);
         }
 
+        public static byte[] SubArray(byte[] array, int head, int length)
+        {
+            return array.Skip(head)
+                        .Take(length)
+                        .ToArray();
+        }
+
+        static void AttackTheEncryption()
+        {
+            byte[] encodingCribWord = StrToBytes(cribWord);
+
+            byte[] q1q2XOR = XOR(firstCiphertext, secondCiphertext);
+
+            for (int i = 0; i < q1q2XOR.Length; i++)
+            {
+                Console.WriteLine(i + "     " + BytesToStr(XOR(encodingCribWord, SubArray(q1q2XOR, i, encodingCribWord.Length))));
+            }
+        }
+
         public static void Main(string[] args)
         {
+            //Console.WriteLine("Enter first ciphertext");
+            //quote1 = StrToBytes(Console.ReadLine());
 
+            //Console.WriteLine("Enter second ciphertext");
+            //quote2 = StrToBytes(Console.ReadLine());
+
+            firstCiphertext = HexStrToBytes("ad924af7a9cdaf3a1bb0c3fe1a20a3f367d82b0f05f8e75643ba688ea2ce8ec88f4762fbe93b50bf5138c7b699");
+            secondCiphertext = HexStrToBytes("a59a0eaeb4d1fc325ab797b31425e6bc66d36e5b18efe8060cb32edeaad68180db4979ede43856a24c7d");
+
+            while(true)
+            {
+                Console.WriteLine("Do you want to enter a new crib word? y/n");
+                if(Console.ReadLine() == "y")
+                {
+                    Console.WriteLine("Enter crib word");
+                    cribWord = Console.ReadLine();
+
+                    AttackTheEncryption();
+                } else
+                {
+                    break;
+                }
+            }
         }
     }
 }
